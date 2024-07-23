b434353cdb99612e59aff69f64ab3be19844afd3
SATYAPRAKASHSWAIN <satyaprakashswain1998@gmail.com>
2024-07-23 20:25:18 +0530

    five commit

--- main.js
+++ main.js
@@ -5,3 +5,35 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+const submit = document.querySelector("#submit");
+
+const saveLead = async () => {
+  // e.preventDefult();
+  let clientName = document.getElementById("required-name").value;
+  let clientEmail = document.getElementById("required-email").value;
+  let clientSubject = document.getElementById("required-subject").value;
+  let clientMessage = document.getElementById("required-message").value;
+
+  alert(`Thankyou for visit our site `);
+  // let response = await fetch("http://localhost:9090/api/getAllData");
+  // console.log(response);
+
+  let response2 = await fetch("http://localhost:9090/api/submit", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: clientName,
+      email: clientEmail,
+      subject: clientSubject,
+      message: clientMessage,
+    }),
+  });
+  console.log(response2);
+  alert(`Thankyou for visit our site `);
+};
+
+submit.addEventListener("click", saveLead);
